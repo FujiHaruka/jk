8685e6fd5fb0266ec3ce858ed813788996799a62
FujiHaruka <fizzyjohn1427@gmail.com>
2022-09-03 22:54:41 +0900

    accept stdin

--- cli.ts
+++ cli.ts
@@ -1,9 +1,9 @@
+import { readAll } from "https://deno.land/std@0.154.0/streams/conversion.ts";
 import { calcRecursiveKeyPaths } from "./JsonKeyPath.ts";
 
-const [file] = Deno.args;
-
-const jsonText = await Deno.readTextFile(file);
-const json = JSON.parse(jsonText);
+const stdinContent = await readAll(Deno.stdin)
+const stdinText = new TextDecoder().decode(stdinContent)
+const json = JSON.parse(stdinText);
 
 console.log(
   calcRecursiveKeyPaths(json).map((path) => path.toString()).join("\n"),
