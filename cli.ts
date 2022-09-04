c4ec946f978ad68da33428d35b0d71d20d28739a
FujiHaruka <fizzyjohn1427@gmail.com>
2022-09-04 16:57:36 +0900

    fmt

--- cli.ts
+++ cli.ts
@@ -1,8 +1,8 @@
 import { readAll } from "https://deno.land/std@0.154.0/streams/conversion.ts";
 import { calcRecursiveKeyPaths } from "./JsonKeyPath.ts";
 
-const stdinContent = await readAll(Deno.stdin)
-const stdinText = new TextDecoder().decode(stdinContent)
+const stdinContent = await readAll(Deno.stdin);
+const stdinText = new TextDecoder().decode(stdinContent);
 const json = JSON.parse(stdinText);
 
 console.log(
